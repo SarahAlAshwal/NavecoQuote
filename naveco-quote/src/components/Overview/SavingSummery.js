1d24341c346297735be9fc489c40e11cee050822
SarahAlAshwal <sa.alashwal@gmail.com>
2020-10-29 21:38:56 -0400

    styling the map

--- naveco-quote/src/components/Overview/SavingSummery.js
+++ naveco-quote/src/components/Overview/SavingSummery.js
@@ -45,29 +45,3 @@ export default function SavingSummery(props) {
 }
 
 
-// {/* <Card className={classes.root} >
-//       <CardContent>
-//         <div className={classes.saving}>
-//           <AttachMoneyIcon fontSize="large"/>
-//           <Typography gutterBottom variant="h5" component="h2">
-//             {`Congratulations!! You can save ${formatNumbers(props.saved)} with this solar system in the next 25 years!`}
-//         </Typography>
-//         </div>
-
-//         <div className={classes.saving}>
-//           <SettingsIcon fontSize="large"/>
-//           <div>
-//           <Typography gutterBottom variant="h5" component="h2">
-//             {`Size: ${props.capacity} KW System`}
-//           </Typography>
-//           {/* <Typography gutterBottom variant="h5" component="h2">
-//             {`Pitch: 25
-//           </Typography> */}
-//           {/* <Typography gutterBottom variant="h5" component="h2">
-//             {`Address: ${props.city}`}
-//           </Typography> */}
-//           </div>
-//         </div>
-        
-//       </CardContent>
-//       </Card> */}
--- naveco-quote/src/components/map.js
+++ naveco-quote/src/components/map.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { Button,TextField , Typography, Box } from '@material-ui/core';
 import './frontPage.css';
+import {Card} from "tabler-react";
 
 //initial web address
 const webaddress = "https://maps.google.com/maps?width=520&height=400&hl=en&t=h&z=19&ie=UTF8&iwloc=B&output=embed&q=%20";
@@ -22,61 +23,81 @@ export default function GoogleMaps(props) {
   }
 
   
- 
-
-//residential
-  const handleAreatypeRes = () => {
-        setResidential(true);
-        document.getElementById("res").style.color = "blue"
-        document.getElementById("com").style.color = "black"
-  }
-//commercial
-  const handleAreatypeCom = () => {
-        setResidential(false);
-        document.getElementById("res").style.color = "black"
-        document.getElementById("com").style.color = "blue"
-  }
 
   return (
-    <main className="wrapper">
-      <Box component="span" ml={50} textAlign="center">
-      <div className="">
+    <Card body = {
+      <>
       <Typography variant="h4">
       Enter your Address
       </Typography>
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+      <TextField
+        id="add"
+        label="Address"
+        style={{ margin: 8 }}
+        placeholder="Enter your address"
+        /*helperText="Full width!"*/
+        fullWidth
+        margin="normal"
+        onChange={props.UpdateAddress}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+      />
+        <Button variant="contained" color="primary" onClick={handleAddress}>Enter</Button>
+      </form>
+      <div>
+       <iframe width="520" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" id="gmap_canvas" className="responsive-iframe"
+      src={finalAddr}>
+       </iframe>
       </div>
-      <section className="">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-        <TextField
-          id="add"
-          label="Address"
-          style={{ margin: 8 }}
-          placeholder="Enter your address"
-          /*helperText="Full width!"*/
-          fullWidth
-          margin="normal"
-          onChange={props.UpdateAddress}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-          <Button color="default" id="res" onClick={handleAreatypeRes}>residential  /</Button>
-          <Button color="default" id="com" onClick={handleAreatypeCom}>commercial</Button> 
-          <Button variant="contained" color="primary" onClick={handleAddress}>Enter</Button>
-        </form>
-        
-        <section className=""> 
-         <iframe width="520" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" id="gmap_canvas" className="responsive-iframe"
-          src={finalAddr}>
-        </iframe>
-        <div className="">
-        <Button variant="contained" color="primary" id="next" onClick = {props.changeMode}>Next</Button>
-        </div>
-        </section>
-      </section>
-      </Box>
-    </main>
+      
+    <Button variant="contained" color="primary" id="next" onClick = {props.changeMode}>Next</Button>
+     </>}
+     />
+
+    
+  
   );
 }
-
+//
+// {/* <main className="wrapper">
+//       <Box component="span" ml={50} textAlign="center">
+//       <div className="">
+//       <Typography variant="h4">
+//       Enter your Address
+//       </Typography>
+//       </div>
+//       <section className="">
+//         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+//         <TextField
+//           id="add"
+//           label="Address"
+//           style={{ margin: 8 }}
+//           placeholder="Enter your address"
+//           /*helperText="Full width!"*/
+//           fullWidth
+//           margin="normal"
+//           onChange={props.UpdateAddress}
+//           InputLabelProps={{
+//             shrink: true,
+//           }}
+//           variant="outlined"
+//         />
+//           <Button color="default" id="res" onClick={handleAreatypeRes}>residential  /</Button>
+//           <Button color="default" id="com" onClick={handleAreatypeCom}>commercial</Button> 
+//           <Button variant="contained" color="primary" onClick={handleAddress}>Enter</Button>
+//         </form>
+        
+//         <section className=""> 
+//          <iframe width="520" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" id="gmap_canvas" className="responsive-iframe"
+//           src={finalAddr}>
+//         </iframe>
+//         <div className="">
+//         <Button variant="contained" color="primary" id="next" onClick = {props.changeMode}>Next</Button>
+//         </div>
+//         </section>
+//       </section>
+//       </Box>
+//     </main> */}
