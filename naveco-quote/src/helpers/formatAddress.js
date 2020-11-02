120ccf2e74ea9d10f10f2a3b2b82393d6b5e18f3
SarahAlAshwal <sa.alashwal@gmail.com>
2020-11-02 09:25:24 -0500

    add comments to functions

--- naveco-quote/src/helpers/formatAddress.js
+++ naveco-quote/src/helpers/formatAddress.js
@@ -1,3 +1,4 @@
+//This function take out spaces between the address words and put + to be used in the API call
 export function formatAddress(address) {
   const addressWithoutSpaces = address.split(' ').filter(word => word.length > 0);
   return addressWithoutSpaces.join('+');
