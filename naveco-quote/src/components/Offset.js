b8afc8f85b4524203d514eb8fb9fc0a6752b800c
SarahAlAshwal <sa.alashwal@gmail.com>
2020-11-02 16:57:01 -0500

    clean Offset file

--- naveco-quote/src/components/Offset.js
+++ naveco-quote/src/components/Offset.js
@@ -9,7 +9,6 @@ import {
 import {Card} from "tabler-react";
 
 export default function Offset (props) {
-	//const [value, setValue] = useState(0.5)
 	console.log('inside offset', props);
 	
 	const stepValue = v => Math.round(v * 10) / 10
@@ -35,26 +34,6 @@ return (
 			</text>
 		</CircularInput>
         </Card.Body>
-      </Card>
-	// <Card title={'Bill Offset'}
-	// className="offset"
-	//  body = {
-	// 	<CircularInput
-	// 	value={stepValue(value)}
-	// 	onChange={v => setValue(stepValue(v))}
-	// 	radius={75}
-		
-	//   >
-	// 		<CircularTrack />
-	// 		<CircularProgress radius = '10px' />
-	// 		<CircularThumb />
-
-	// 		<text x={80} y={80} textAnchor="middle" dy="0.3em" fontWeight="bold" fontSize="14">
-	// 			{Math.round(stepValue(value) * 100)}%
-	// 		</text>
-	// 	</CircularInput>
-//	}/>
-	
-    
+      </Card>    
 	);
 }
